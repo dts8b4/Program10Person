2cc9643e987b465a2a083bca521fb14f87038c20
dts8b4 <157173150+dts8b4@users.noreply.github.com>
2024-03-17 11:49:58 -0500

    Update main.cpp
    Added compile instructions and added more to main docstring

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@ Name: Dylan Sullins
 Email: dts8b4@umsystem.edu
 Date: 03/17/2024
 Program 10: Person Class
+
+All work either provided by the instructor or written by Dylan Sullins in VSCode and compiled with G++.
+
+Compiles without error using G++ version 10.3.1 20210520 on a Windows 11 Desktop.
+To replicate the process, simply open the directory in a command prompt and enter the following commands: 
+    "g++ *.cpp -o main" followed by "./main" 
+The output.txt will be populated with the results. 
+
 Main Program
     Create vectors for generic people, students, 
         employees, teachers, and all people
